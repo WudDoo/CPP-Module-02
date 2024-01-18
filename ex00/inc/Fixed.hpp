769d6ad2c6dc94fc9eadfb96fed3ef05c02120ec
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-18 15:57:52 +0000

    ex00: Changed variable names in Fixed.hpp

--- ex00/inc/Fixed.hpp
+++ ex00/inc/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:17:54 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/16 15:52:27 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/01/18 15:56:40 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ class Fixed {
 
 	public:
 		Fixed( void );
-		Fixed( const Fixed &fixed );
+		Fixed( const Fixed &src );
 		~Fixed( void );
-		Fixed & operator = ( const Fixed &fixed );
-		
+		Fixed & operator = ( const Fixed &src );
+
 		int	getRawBits( void ) const;
 		void	setRawBits( int const raw );
 };
