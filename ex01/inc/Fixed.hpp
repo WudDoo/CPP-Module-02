2a87032812028f8b0eff1cf2390bbf9b33f99ded
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-18 16:33:16 +0000

    ex01: Added new public member constructor and function prototypes for Fixed class

--- ex01/inc/Fixed.hpp
+++ ex01/inc/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:17:54 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/16 15:52:27 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/01/18 16:32:15 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,22 @@ class Fixed {
 
 	public:
 		Fixed( void );
-		Fixed( const Fixed &fixed );
+		Fixed( const Fixed &src );
+		Fixed( const int integer );
+		Fixed( const float number );
+
 		~Fixed( void );
-		Fixed & operator = ( const Fixed &fixed );
-		
+
+		Fixed & operator = ( const Fixed &src );
+
 		int	getRawBits( void ) const;
 		void	setRawBits( int const raw );
+
+		float	toFloat( void ) const;
+		int	toInt( void ) const;
 };
+	// And add the following function to the Fixed class files:
+	// • An overload of the insertion («) operator that inserts a floating-point representation
+	// of the fixed-point number into the output stream object passed as parameter.
 
 #endif
