9dde67a9ef310161e6d7d14c028f314dfeff6529
WudDoo <89762267+WudDoo@users.noreply.github.com>
2024-01-18 15:40:37 +0000

    ex00: Added protection on copy assignment operator

--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:49:53 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/16 18:16:47 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/01/18 15:31:13 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,19 @@ Fixed::Fixed( void ) {
 	this->numberValue = 0;
 }
 
-Fixed::Fixed( const Fixed &fixed ) {
+Fixed::Fixed( const Fixed &src ) {
 	std::cout << "Copy constructor called" << std::endl;
-	*this = fixed;
+	*this = src;
 }
 
 Fixed::~Fixed( void ) {
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed& Fixed::operator= ( const Fixed &fixed ) {
+Fixed& Fixed::operator= ( const Fixed &src ) {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->setRawBits(fixed.getRawBits());
+	if (this != &src)
+		this->setRawBits(src.getRawBits());
 	return (*this);
 }
 
