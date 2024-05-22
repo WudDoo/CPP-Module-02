978fe3a4b9a8de32db39facd55d0bf4195e73d2d
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-22 14:20:40 +0100

    ex00: Added more tests to main.cpp

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:33:17 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/18 15:43:50 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:19:50 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #include "../inc/Fixed.hpp"
 
 int	main( void ) {
-
+	{
+	std::cout << "\e[90m\e[107mSubject tests:\e[0m" << std::endl;
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
@@ -24,6 +25,21 @@ int	main( void ) {
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "\e[90m\e[107mMy tests:\e[0m" << std::endl;
+		Fixed a;
+		a.setRawBits(12);
 
-	return 0;
+		Fixed b( a );
+		Fixed c;
+
+		c.setRawBits(14);
+		c = b;
+
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
 }
