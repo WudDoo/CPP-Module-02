ba2170993bc7a166d0e23a9071493da11417b94d
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-22 15:42:21 +0100

    ex01: Added comments to Fixed.hpp

--- ex01/inc/Fixed.hpp
+++ ex01/inc/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:17:54 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/19 13:31:51 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/22 15:41:53 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,25 @@ class Fixed {
 		static const int	fractionalBits = 8;
 
 	public:
+		// Constructors
 		Fixed( void );
 		Fixed( const Fixed &src );
 		Fixed( const int integer );
 		Fixed( const float number );
 
+		// Destructor
 		~Fixed( void );
 
+		// Copy assignment operator
 		Fixed & operator = ( const Fixed &src );
 
+		// Getter
 		int	getRawBits( void ) const;
+
+		// Setter
 		void	setRawBits( int const raw );
 
+		// Methods
 		float	toFloat( void ) const;
 		int	toInt( void ) const;
 
