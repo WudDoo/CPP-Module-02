c4f593eed3e9eae14d14c8426f7fba2799a1addd
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-27 12:49:55 +0100

    ex01: Added header text to main.cpp

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:45:44 by mortins-          #+#    #+#             */
-/*   Updated: 2024/01/19 13:33:16 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/27 12:49:15 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 #include <iostream>
 
 int	main( void )	{
+	{
+		std::cout << "\e[90m\e[107mSubject tests:\e[0m" << std::endl;
+		Fixed	a;
+		Fixed const	b( 10 );
+		Fixed const	c( 42.42f );
+		Fixed const	d( b );
 
-	Fixed	a;
-	Fixed const	b( 10 );
-	Fixed const	c( 42.42f );
-	Fixed const	d( b );
+		a = Fixed( 1234.4321f );
 
-	a = Fixed( 1234.4321f );
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	}
 	return 0;
 }
